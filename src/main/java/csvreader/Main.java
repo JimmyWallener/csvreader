dd1017f4e2b9ba1b1e4c281f1782deeefe0d9fac
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-10-07 16:28:43 +0200

    Added function for adding records & saving to file.

--- src/main/java/csvreader/Main.java
+++ src/main/java/csvreader/Main.java
@@ -1,10 +1,12 @@
 package csvreader;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 
 import java.io.IOException;
@@ -18,6 +20,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("table.fxml"));
         stage.setScene(new Scene(root));
         stage.setTitle("CSV Reader");
+        stage.setOnCloseRequest(windowEvent -> Platform.exit());
         stage.show();
 
 
